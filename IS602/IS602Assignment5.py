fafc174fcbc8e3067854ec47c4313fc36f1ea042
dstern04 <david.c.stern@gmail.com>
2015-10-07 17:08:49 -0400

    update

---
+++ IS602/IS602Assignment5.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+# In[72]:
+
+import csv
+import urllib2
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/dstern04/MSDA-Work/master/IS602/brainandbody.csv'
+response = urllib2.urlopen(url)
+data = pd.read_csv(response)
+
+print data
+
+
+# In[86]:
+
+sumY = data.sum(axis=0)[1]
+sumX = data.sum(axis=0)[2]
+sumXY = (data["brain"]*data["body"]).sum()
+sumX2 = (data["brain"]**2).sum()
+meanX = data["brain"].mean()
+meanY = data["body"].mean()
+a = (sumY*sumX2-sumX*sumXY)/(len(data)*sumX2-sumX**2)
+b = (sumXY - len(data)*meanX*meanY)/(sumX2 - len(data)*meanX**2)
+
+
+# In[90]:
+
+print "The least squares fit for this data gives us the model:  body = ", b, "* brain ", a
+
